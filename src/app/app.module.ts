912c7f61e61775f92aa5e6cce92f9ca20928ae2f
[your <manishkanvadiya@gmail.com>
2021-08-23 23:28:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SiderbarComponent } from './component/siderbar/siderbar.component';
+import { MainComponent } from './component/main/main.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SiderbarComponent,
+    MainComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
